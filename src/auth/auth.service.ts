3622af137ad35f5a9597bf015658add0c1124d8a
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-11-24 15:21:18 +0100

    test: login error

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -18,7 +18,7 @@ export class AuthService {
     const user = await this.prisma.user.findUnique({
       where: { username: body.username, },
     });
-    const valid = await argon.verify(user?.hash, body?.password);
+    const valid = await argon.verify(user.hash, body.password);
     if (!valid || !user) throw new ForbiddenException('Invalid credentials');
     return this.signToken(user.id, user.username);
   }
@@ -27,6 +27,8 @@ export class AuthService {
 
     const hash = await argon.hash(body.password);
     try {
+      const hash = await argon.hash(body.password);
+
       const user = await this.prisma.user.create({
         data: {
           username: body.username,
