2fa579a81c6acbe7df854c90a4c7fca17e9e3252
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-11-28 19:53:28 +0100

    fix: doesn't crash when user is not found

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -18,8 +18,9 @@ export class AuthService {
     const user = await this.prisma.user.findUnique({
       where: { username: body.username, },
     });
-    const valid = await argon.verify(user.hash, body.password);
-    if (!valid || !user) throw new ForbiddenException('Invalid credentials');
+    if (!user) throw new ForbiddenException('Invalid credentials');
+    const valid = await argon.verify(user?.hash, body?.password);
+    if (!valid) throw new ForbiddenException('Invalid credentials');
     return this.signToken(user.id, user.username);
   }
 
