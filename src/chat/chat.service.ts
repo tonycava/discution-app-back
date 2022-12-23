d2456d1dfa5cdaa469cfd44521c3296e7432f397
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-12-24 00:10:48 +0100

    test: handle error in case no group is created yet

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -5,32 +5,33 @@ import { ChatDto, Limit } from './dto/chat.dto';
 @Injectable()
 export class ChatService {
   constructor(private prisma: PrismaService) {}
-  
+
   getChats({ start, end }: Limit) {
     return this.prisma.chat.findMany({
-      skip: +start,
-      take: +end,
+      skip: + start,
+      take: + end,
       select: {
         message: true,
-        userId: true
+        userId: true,
       },
       orderBy: {
-        createdAt: 'desc'
-      }
+        createdAt: 'desc',
+      },
     });
   }
-  
+
   addChat({ message, userId }: ChatDto) {
     return this.prisma.chat.create({
       data: {
         message,
-        user: {
-          connect: { id: userId }
-        },
+        user: { connect: { id: userId } },
         group: {
-          connect: { id: '7035ae37-f630-4697-ab17-4d516eaba7d7' }
-        }
-      }
+          connectOrCreate: {
+            create: { name: 'general', id: '7035ae37-f630-4697-ab17-4d516eaba7d7' },
+            where: { id: '7035ae37-f630-4697-ab17-4d516eaba7d7' },
+          },
+        },
+      },
     });
   }
 }
